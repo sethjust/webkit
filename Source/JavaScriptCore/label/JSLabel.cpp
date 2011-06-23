c83305341b185cc3ec1ebe5e8c2f1946abff3c41
Seth Just <seth.just@gmail.com>
2011-06-23 12:07:03 -0600

    added label directory to git

---
+++ Source/JavaScriptCore/label/JSLabel.cpp
@@ -0,0 +1,44 @@
+/*
+ *  JSLabel.cpp
+ *  JavaScriptCore
+ *
+ *  Created by Alan Cleary on 6/23/11.
+ *  Copyright 2011 Alan Cleary and Seth Just. All rights reserved.
+ *
+ */
+
+#include "config.h"
+#include "JSLabel.h"
+
+namespace JSC {
+	
+	// JSLabel constructor
+	JSLabel::JSLabel() {
+		label = new long;
+		*label = 0;
+	}
+	
+	// JSLabel constructor overload; Takes value
+	JSLabel::JSLabel(long l) {
+		label = new long;
+		*label = l;
+	}
+	
+	/*
+	 // Deconstruct JSLabel
+	 JSLabel::~JSLabel() {
+	 delete label;
+	 }
+	 */
+	
+	// Get labels value
+	long JSLabel::Val() {
+		return *label;
+	}
+	
+	// OR two labels
+	JSLabel JSLabel::Join(JSLabel l) {
+		return JSLabel (l.Val() | this->Val());
+	}
+	
+}
---
+++ Source/JavaScriptCore/label/JSLabel.h
@@ -0,0 +1,27 @@
+/*
+ *  JSLabel.h
+ *  JavaScriptCore
+ *
+ *  Created by Alan Cleary on 6/23/11.
+ *  Copyright 2011 Alan Cleary and Seth Just. All rights reserved.
+ *
+ */
+
+#ifndef JSLabel_h
+#define JSLabel_h
+
+namespace JSC {
+	
+	class JSLabel {
+		long *label; // label's value
+	public:
+		JSLabel(); // constructor
+		JSLabel(long); // overload
+		long Val(); // return label value
+		//~JSLabel(); // Deconstructor
+		JSLabel Join(JSLabel l); // join labels
+	};
+	
+}
+
+#endif // JSLabel_h
